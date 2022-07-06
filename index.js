d45ec649973e453c6899cfd46769976bf573ed64
jeffreychuka <87776403+jeffreychuka@users.noreply.github.com>
2022-07-06 10:03:28 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,20 @@
+//javascript
+
+
+
+let firstname="Jeff";
+let middlename="cerdo";
+let lastname="chuka";
+let age =30;
+let today=new Date();
+console.log(today);
+let currentyear=today.getFullYear();
+console.log(currentyear)
+let birthdate=currentyear-age
+
+
+console.log("HELLO! My Name is", firstname + " "  +    middlename  + " " +  lastname);
+console.log("I was born in " + birthdate + ".");
+
+//
+
